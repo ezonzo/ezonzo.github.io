709fc6ef876bfc43b13cdf77df14c68210a8e9bb
roberto <robertocarta.mail@gmail.com>
2020-06-20 12:29:50 +0200

    Updates

---
+++ precache-manifest.40b324fd78b3fd66b8d6a8e6016afb6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223412ee21844ae4b67cd10fccdf1077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "009022fa2742d56eee8d",
+    "url": "/static/css/main.d280d06f.chunk.css"
+  },
+  {
+    "revision": "a704fad1922e355fe0c0",
+    "url": "/static/js/2.b202cb6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b202cb6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009022fa2742d56eee8d",
+    "url": "/static/js/main.99782ff5.chunk.js"
+  },
+  {
+    "revision": "fd1d6c59724620db742d",
+    "url": "/static/js/runtime-main.35ea6e92.js"
+  },
+  {
+    "revision": "2b75c51d7b9cf36ff19edac5539961ab",
+    "url": "/static/media/EZONZO.2b75c51d.svg"
+  },
+  {
+    "revision": "01292903163cc7ee36b5ef44ea74c831",
+    "url": "/static/media/bolognalandscape.01292903.jpg"
+  }
+]);
